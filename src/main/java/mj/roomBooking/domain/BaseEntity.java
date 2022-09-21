3ca9a65fd252f1b705a325c9c2931364d7225176
MJbae <bmj5208@gmail.com>
2022-09-21 21:00:28 +0900

    Add Parent Table Annotation for Joinning

--- src/main/java/mj/roomBooking/domain/BaseEntity.java
+++ src/main/java/mj/roomBooking/domain/BaseEntity.java
@@ -1,18 +1,22 @@
 package mj.roomBooking.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@Getter
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
 
     @CreationTimestamp
     private LocalDateTime createdAt;
