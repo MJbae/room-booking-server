2c6a7624abdf9bed9f9b5de04e683bb8115bf9ea
MJbae <bmj5208@gmail.com>
2022-09-23 20:15:55 +0900

    Define MemberRepository as stub

---
+++ src/main/java/mj/roomBooking/infra/MemberRepository.java
@@ -0,0 +1,12 @@
+package mj.roomBooking.infra;
+
+import mj.roomBooking.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
+
+    void deleteByName(String name);
+}
