b2b287ed6e7e28af76b4cfffd0ae734732c1b23f
MJbae <bmj5208@gmail.com>
2022-09-23 21:03:50 +0900

    Add test for checking return values from MemberService::createWith

--- src/test/java/mj/roomBooking/application/MemberServiceTest.java
+++ src/test/java/mj/roomBooking/application/MemberServiceTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 @DisplayName("MemberService")
@@ -25,6 +27,7 @@ class MemberServiceTest {
     private final String MEMBER_NAME = "배만진";
     private final String GROUP_NAME = "OGQ";
     private final String TEAM_NAME = "모바일본부";
+    private Member member;
     private Team team;
     private Group group;
 
@@ -33,18 +36,36 @@ class MemberServiceTest {
         service = new MemberService(memberRepository);
         group = new Group(GROUP_NAME);
         team = new Team(TEAM_NAME, group);
-        teamRepository.save(team);
+        member = new Member(MEMBER_NAME, team);
     }
 
     @Nested
     @DisplayName("createWith 메소드는")
     class Describe_createWith {
-
+        @BeforeEach
+        void setUp() {
+            given(memberRepository.save(any())).willReturn(member);
+        }
         @Test
         @DisplayName("정확한 사용자 이름을 반환한다")
         void it_returns_exact_name() {
             Member memberCreated = service.createWith(MEMBER_NAME, team);
             assertThat(memberCreated.getName()).isEqualTo(MEMBER_NAME);
+        }
+
+        @Test
+        @DisplayName("정확한 Team Entity 반환한다")
+        void it_returns_exact_team() {
+            Member memberCreated = service.createWith(MEMBER_NAME, team);
+            assertThat(memberCreated.getTeam()).isEqualTo(team);
+        }
+
+        @Test
+        @DisplayName("정확한 Team, Group 이름을 반환한다")
+        void it_returns_exact_team_name_and_group_name() {
+            Member memberCreated = service.createWith(MEMBER_NAME, team);
+            assertThat(memberCreated.getTeam().getName()).isEqualTo(TEAM_NAME);
+            assertThat(memberCreated.getTeam().getGroup().getName()).isEqualTo(GROUP_NAME);
         }
     }
 }
