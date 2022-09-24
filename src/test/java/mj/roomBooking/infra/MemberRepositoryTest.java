aab497e29cd739f40e0d1ccb4958e028066803f0
MJbae <bmj5208@gmail.com>
2022-09-24 23:47:25 +0900

    Remove useless test setting

--- src/test/java/mj/roomBooking/infra/MemberRepositoryTest.java
+++ src/test/java/mj/roomBooking/infra/MemberRepositoryTest.java
@@ -16,10 +16,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(properties = {
-        "spring.jpa.hibernate.ddl-auto=create"
-})
-
 @DisplayName("MemberRepository")
 class MemberRepositoryTest {
     @Autowired
