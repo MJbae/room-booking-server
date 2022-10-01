757c060f7669fb757d381c1659558cd5645d79de
MJbae <bmj5208@gmail.com>
2022-10-01 11:25:42 +0900

    Apply JPQL in OrganizationRepository::findById

--- src/main/java/mj/roomBooking/infra/OrganizationRepository.java
+++ src/main/java/mj/roomBooking/infra/OrganizationRepository.java
@@ -2,12 +2,17 @@ package mj.roomBooking.infra;
 
 import mj.roomBooking.domain.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-    Optional<Organization> findById(Long Id);
+    @Query(value = "select o from Organization o " +
+            "join fetch o.teams " +
+            "where o.id = :id")
+    Optional<Organization> findById(@Param("id") Long Id);
 
     void deleteById(Long Id);
 }
